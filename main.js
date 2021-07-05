48e339a66cb9c751ad4cf329dbe46188ff333fe6
kimothokamau <kimothokamau@gmail.com>
2021-07-05 15:09:39 -0600

    Create div for newbook and add remove function

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let author = document.getElementById('author');
 let form = document.querySelector('#form');
 let bookContainer = document.getElementById('book-container');
 let addBtn = document.getElementById('addBtn');
+let rmvBtn;
 
 // console.log(addBtn);
 
@@ -12,30 +13,51 @@ function Book(title,author) {
     this.author = author;
 }
 
-let book = new Book();
+// let book = new Book();
 
 function addBook(book){
     displayBooks.push(book);
 }
 
 function createBook(book){
+    let bookDiv = document.createElement('div');
     let bookTitle = document.createElement('h3');
     bookTitle.textContent = title.value;
+    bookDiv.append(bookTitle)
     let bookAuthor = document.createElement('h3');
     bookAuthor.textContent = author.value;
-    bookContainer.append(bookTitle,bookAuthor)
+    // bookContainer.append(bookTitle,bookAuthor)
+    bookDiv.append(bookAuthor);
     let removeBtn = document.createElement('button');
+    removeBtn.className = 'remv-cls';
     removeBtn.textContent = 'remove'
-    bookContainer.append(removeBtn)
+    bookDiv.append(removeBtn);
+    bookContainer.append(bookDiv);
+    rmvBtn = document.querySelector('.remv-cls');
 }
 
 form.addEventListener('submit', (event) => {
     // ...
     // stop form submission
     event.preventDefault();
+    let book = new Book(title.value, author.value);
     createBook(book);
-    console.log(createBook(book));
+    addBook(book);
+    // console.log(createBook(book));
+    console.log(displayBooks);
   
 });
 
-console.log(document.forms);
+function removeBook(el) {
+  if (el.classList.contains('remv-cls')) {
+    el.parentElement.parentElement.remove();
+  }
+
+}
+
+rmvBtn.addEventListener('click', function(e) {
+  console.log(e);
+  removeBook(e.target)});
+
+
+// console.log(document.forms);
