a0d717137196eb0636af9ab04efc8f6f3301f786
Jocyline Magero <jmagero2017@gmail.com>
2021-07-05 12:59:45 -0500

    Add function to add book to collection

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+let title = document.getElementById('title').value;
+let author = document.getElementById('author').value;
 let displayBooks = [];
 function Book(title,author) {
     this.title = title;
     this.author = author;
 }
+
+let book = new Book(title,author);
+
+function addBook(book){
+    displayBooks.push(book);
+}
+
