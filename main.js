1e1fc21a024cdee2ab2828321ed92d48e54f0589
Jocyline Magero <jmagero2017@gmail.com>
2021-07-05 14:50:20 -0500

    AddEventListener to display book

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-let title = document.getElementById('title').value;
-let author = document.getElementById('author').value;
-let form = document.getElementById('form')
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let form = document.querySelector('#form');
 let bookContainer = document.getElementById('book-container');
+let addBtn = document.getElementById('addBtn');
+
+// console.log(addBtn);
 
 let displayBooks = [];
 function Book(title,author) {
@@ -9,16 +12,30 @@ function Book(title,author) {
     this.author = author;
 }
 
-let book = new Book(title,author);
+let book = new Book();
 
 function addBook(book){
     displayBooks.push(book);
 }
 
-function displayBooks(book){
-    let bookAuthor = document.createElement('h3');
-    bookAuthor.textContent = author;
+function createBook(book){
     let bookTitle = document.createElement('h3');
-    bookTitle.textContent = title;
-    bookContainer.append(bookAuthor,bookTitle)
+    bookTitle.textContent = title.value;
+    let bookAuthor = document.createElement('h3');
+    bookAuthor.textContent = author.value;
+    bookContainer.append(bookTitle,bookAuthor)
+    let removeBtn = document.createElement('button');
+    removeBtn.textContent = 'remove'
+    bookContainer.append(removeBtn)
 }
+
+form.addEventListener('submit', (event) => {
+    // ...
+    // stop form submission
+    event.preventDefault();
+    createBook(book);
+    console.log(createBook(book));
+  
+});
+
+console.log(document.forms);
