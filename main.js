08267d4b509ec313a3e5f7f2cba5fa31d2094d0d
Jocyline Magero <jmagero2017@gmail.com>
2021-07-05 13:00:58 -0500

    Add displayBook function

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let title = document.getElementById('title').value;
 let author = document.getElementById('author').value;
+let form = document.getElementById('form')
+let bookContainer = document.getElementById('book-container');
+
 let displayBooks = [];
 function Book(title,author) {
     this.title = title;
@@ -12,3 +15,10 @@ function addBook(book){
     displayBooks.push(book);
 }
 
+function displayBooks(book){
+    let bookAuthor = document.createElement('h3');
+    bookAuthor.textContent = author;
+    let bookTitle = document.createElement('h3');
+    bookTitle.textContent = title;
+    bookContainer.append(bookAuthor,bookTitle)
+}
