330efcafe12cd1abf6bac104493d846b125719e8
kimothokamau <kimothokamau@gmail.com>
2021-07-06 15:24:32 -0600

    Refractor methods to classes

--- main.js
+++ main.js
@@ -1,8 +1,50 @@
-const form = document.querySelector('#form');
-const bookContainer = document.getElementById('book-container');
+// Book class
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
+// Library/display class
 let library = [];
-function getBooks() {
+class Library {
+  static displayBooks() {
+    const data = Storage.getBooks();
+    data.forEach((book) => {
+      Library.createBook(book);
+    });
+  }
+
+  static createBook(book) {
+    const bookDiv = document.createElement('div');
+    bookDiv.innerHTML = `<p>${book.title} by ${book.author}</p>
+                        <button class ='remv-cls'>Remove</button>`;
+    bookContainer.appendChild(bookDiv);
+  }
+
+  static removeBook(element) {
+    const books = Storage.getBooks();
+    const indexBook = Array.prototype.indexOf.call(
+      bookContainer.childNodes,
+      element.parentElement,
+    );
+  
+    if (element.classList.contains('remv-cls')) {
+      books.forEach((book, index) => {
+        if (indexBook === index) {
+          books.splice(index, 1);
+        }
+        library = books;
+        localStorage.setItem('library', JSON.stringify(library));
+      });
+      element.parentElement.remove();
+    }
+  }
+}
+
+class Storage {
+  static getBooks() {
   if (localStorage.getItem('library')) {
     library = JSON.parse(localStorage.getItem('library'));
   } else {
@@ -11,62 +53,40 @@ function getBooks() {
   return library;
 }
 
-function setBook(book) {
-  const books = getBooks();
+static setBook(book) {
+  const books = Storage.getBooks();
   books.push(book);
   library = books;
   localStorage.setItem('library', JSON.stringify(books));
 }
-
-function createBook(book) {
-  const bookDiv = document.createElement('div');
-  bookDiv.innerHTML = `<p>${book.title} by ${book.author}</p>
-                      <button class ='remv-cls'>Remove</button>`;
-  bookContainer.appendChild(bookDiv);
 }
+
+// Event listeners
+const form = document.querySelector('#form');
+const bookContainer = document.getElementById('book-container');
+
+document.addEventListener('DOMContentLoaded', () => {
+  Library.displayBooks();
+});
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   if (title !== '' && author !== '') {
-    const book = { title: `${title}`, author: `${author}` };
-    createBook(book);
-    setBook(book);
+    const book = new Book(title, author);
+    // const book = { title: `${title}`, author: `${author}` };
+    Library.createBook(book);
+    Storage.setBook(book);
     form.reset();
   }
 });
 
-function removeBook(element) {
-  const books = getBooks();
-  const indexBook = Array.prototype.indexOf.call(
-    bookContainer.childNodes,
-    element.parentElement,
-  );
-
-  if (element.classList.contains('remv-cls')) {
-    books.forEach((book, index) => {
-      if (indexBook === index) {
-        books.splice(index, 1);
-      }
-      library = books;
-      localStorage.setItem('library', JSON.stringify(library));
-    });
-    element.parentElement.remove();
-  }
-}
 
 bookContainer.addEventListener('click', (e) => {
-  removeBook(e.target);
+  Library.removeBook(e.target);
 });
 
-function displayBooks() {
-  const data = getBooks();
-  data.forEach((book) => {
-    createBook(book);
-  });
-}
 
-document.addEventListener('DOMContentLoaded', () => {
-  displayBooks();
-});
+
+
