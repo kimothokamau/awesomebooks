00e90b639926f84904e0077cdeb9cff0f59f320c
Jocyline Magero <jmagero2017@gmail.com>
2021-07-06 12:02:21 -0500

    Add function to remove remove book from localStorage

--- main.js
+++ main.js
@@ -1,62 +1,72 @@
-const title = document.getElementById('title');
-const author = document.getElementById('author');
 const form = document.querySelector('#form');
 const bookContainer = document.getElementById('book-container');
 
-
 let  library = [];
-if (localStorage.getItem('library')) {
+function getBooks(){
+  if (localStorage.getItem('library')) {
     library = JSON.parse(localStorage.getItem('library'))
   } else {
     library = []
+  }
+  return library
 }
 
-const data = JSON.parse(localStorage.getItem('library'));
-
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
+function setBook(book){
+  const books = getBooks();
+  books.push(book);
+  library = books;
+  localStorage.setItem('library',JSON.stringify(books));
 }
 
 function addBook(book) {
   library.push(book);
 }
 
-function createBook() {
+function createBook(book) {
   const bookDiv = document.createElement('div');
-  const bookTitle = document.createElement('h3');
-  bookTitle.textContent = title.value;
-  bookDiv.append(bookTitle);
-  const bookAuthor = document.createElement('h3');
-  bookAuthor.textContent = author.value;
-  bookDiv.append(bookAuthor);
-  const removeBtn = document.createElement('button');
-  removeBtn.className = 'remv-cls';
-  removeBtn.textContent = 'remove';
-  bookDiv.append(removeBtn);
-  bookContainer.append(bookDiv);
+  bookDiv.innerHTML = `<p>${book.title} by ${book.author}</p>
+                      <button class ='remv-cls'>Remove</button>`
+  bookContainer.appendChild(bookDiv);
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const book = new Book(title.value, author.value);
-  addBook(book);
-  localStorage.setItem('library',JSON.stringify(library));
-  createBook(book);
-  form.reset();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  if (title !== '' && author !== '') {
+    const book = { title: `${title}`, author: `${author}` };
+    createBook(book);
+    setBook(book);
+    form.reset();
+  }
 });
 
-function removeBook(el) {
-  if (el.classList.contains('remv-cls')) {
-    el.parentElement.remove();
+function removeBook(element) {
+  const books = getBooks();
+  const indexBook = Array.prototype.indexOf.call(
+    bookContainer.childNodes,
+    element.parentElement,
+  );
+
+  if (element.classList.contains('remv-cls')) {
+    books.forEach((book,index) => {
+      if(indexBook === index){
+        books.splice(index,1)
+      }
+      library = books;
+      localStorage.setItem('library', JSON.stringify(library));
+    });
+    element.parentElement.remove();
   }
 }
+
 
 bookContainer.addEventListener('click', (e) => {
   removeBook(e.target);
 });
 
 function displayBooks(){
+  const data = getBooks();
   data.forEach(book => {
     createBook(book);
   });
